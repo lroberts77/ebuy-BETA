33b11431579af8837256c65f00b5085b8e89f6e9
Liam R <liamroberts77@googlemail.com>
2022-03-15 16:53:38 +0000

    imported react router into app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
+import { BrowserRouter as Router, Switch, Router, Route } from 'react-router-dom';
 
 function App() {
   return (
