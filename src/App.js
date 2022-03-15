886fb37a69cc35b3f595783349d749b20846d271
Liam R <liamroberts77@googlemail.com>
2022-03-15 16:45:46 +0000

    added boilerplate component in productListing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <Header />
+    <div className='App'>
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/productListing/productListing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const productListing = () => {
+  return (
+    <div>productListing</div>
+  )
+}
+
+export default productListing
