c4d83ccbce73750d327a2b56f18f1cc731c22506
Liam R <liamroberts77@googlemail.com>
2022-03-16 16:04:01 +0000

    added axios api fetch for single product using productid

--- src/components/productDetails/productDetails.js
+++ src/components/productDetails/productDetails.js
@@ -3,6 +3,16 @@ import axios from 'axios';
 import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
+ 
+  const { productId } = useParams();
+  console.log(productId);
+
+  const fetchProductDetail = async () => {
+    const res = await axios.get(`https://fakestoreapi.com/products${productId}`)      
+    .catch((err) => {
+    console.log("error", err);
+    });
+  }
   return (
     <div>
       ProductDetails
