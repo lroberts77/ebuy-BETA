6fb369ab1c1c17016a4078f36dfb53207e37de2b
Liam R <liamroberts77@googlemail.com>
2022-03-16 16:20:41 +0000

    added useffect to call api fetch on productid change

--- src/components/productDetails/productDetails.js
+++ src/components/productDetails/productDetails.js
@@ -12,12 +12,19 @@ const ProductDetails = () => {
   console.log(productId);
 
   const fetchProductDetail = async () => {
-    const res = await axios.get(`https://fakestoreapi.com/products${productId}`)      
+    const res = await axios.get(`https://fakestoreapi.com/products/${productId}`)      
     .catch((err) => {
     console.log("error", err);
     });
     dispatch(selectedProduct(res.data))
   }
+
+  useEffect(() => {
+    if (productId && productId !== '')
+    fetchProductDetail()
+  }, [productId])
+  console.log(product);
+  
 
   return (
     <div>
