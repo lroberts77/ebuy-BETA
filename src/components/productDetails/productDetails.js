d54ea41c7d058c156dfb1876f376b1b4a1cef78e
Liam R <liamroberts77@googlemail.com>
2022-03-16 15:57:05 +0000

    imported axios and useparams into productdetails.js

--- src/components/productDetails/productDetails.js
+++ src/components/productDetails/productDetails.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
   return (
